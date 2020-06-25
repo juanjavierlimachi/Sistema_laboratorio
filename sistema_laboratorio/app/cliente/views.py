4428c48075189d44a7ecc2240ccebe95d552b40a
PcDestok <codemar.potosi@gmail.com>
2020-06-25 12:52:00 -0400

    create report for analysis results

--- sistema_laboratorio/app/cliente/views.py
+++ sistema_laboratorio/app/cliente/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect,HttpResponse,HttpResponseRedirect, get_object_or_404
 from sistema_laboratorio.app.cliente.forms import *
-from django.shortcuts import redirect
+import calendar
+from datetime import datetime, date, time, timedelta
+from django.views.generic import View
+from io import StringIO
+import os
+import io
+from xhtml2pdf import pisa
+from django.template.loader import render_to_string
+
+#de reporlab
+from reportlab.pdfgen import canvas
+from django.http import FileResponse
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, cm
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+
 # Create your views here.
 def RegistrarCli(request):
 	datos = Cliente.objects.all().order_by('-id')
@@ -158,23 +173,105 @@ def printCertify(request, idProductos):
 	idProductos = list(map(int,idProductos))
 	productos = Producto.objects.filter(estado = True)
 	resultados = Resultado.objects.filter(estado = True).order_by('Elemento')
-
-	#consulta = Producto.objects.raw('SELECT * FROM clientes_productos')
 	for id_producto in idProductos:
 		getProduct = Producto.objects.get(id = int(id_producto))
-		print(id_producto)
-		results=Resultado.objects.filter(producto_id=getProduct.id)
-		#print(results)
-		#(getProduct,results)
-		
-	return render(request,'cliente/printCertify.html',{'idProductos':idProductos, 'productos':productos, 'resultados':resultados,'getProduct':getProduct})
+		break
+	date_today = datetime.now().strftime("%d/%m/%Y")
+	print(request.user.first_name)
+	dic = {
+			'pagesize':'letter',
+			'idProductos':idProductos, 
+			'productos':productos,
+			'resultados':resultados,
+			'getProduct':getProduct,
+			'date_today':date_today,
+			'usuario':request.user.first_name.title()
+		 }
+	html=render_to_string('cliente/printCertify.html',dic)
+	return generar_pdf(html)
 
-
-def showCertify(getProduct):
-	results=Resultado.objects.filter(producto_id=getProduct.id)
-	print('holas')
-	return HttpResponse(results)
+def generar_pdf(html):
+	resultado=io.BytesIO()
+	pdf=pisa.pisaDocument(io.BytesIO(html.encode("UTF:8")),resultado)
+	if not pdf.err:
+		return HttpResponse(resultado.getvalue(),'application/pdf')
+	return HttpResponse("Error al generar el reporte")
 
 def detailReport(request):
 	clients=Cliente.objects.filter(estado=True)
 	return render(request,'cliente/detailReport.html',{'clients':clients})
+
+class ReportAnalisis(View):
+	def cabecera(self,pdf):
+		#Utilizamos el archivo logo_django.png que está guardado en la carpeta media/imagenes
+		#archivo_imagen = settings.MEDIA_ROOT+'/imagenes/logo_django.png'
+		#Definimos el tamaño de la imagen a cargar y las coordenadas correspondientes
+		#pdf.drawImage(archivo_imagen, 40, 750, 120, 90,preserveAspectRatio=True)
+		#Establecemos el tamaño de letra en 16 y el tipo de letra Helvetica
+		pdf.setFont("Helvetica", 18)
+		#Dibujamos una cadena en la ubicación X,Y especificada
+		pdf.drawString(230, 790, u"REPORTE DE")
+		pdf.setFont("Helvetica", 18)
+		pdf.drawString(245, 770, u"ANÁLISIS")
+	def tabla(self,pdf,y, idProductos):
+		idProductos = idProductos.split(',')
+		idProductos = list(idProductos)
+		#Creamos una tupla de encabezados para neustra tabla
+		encabezados = ('Fecha', 'Lote', 'Resultados', '')
+		#Creamos una lista de tuplas que van a contener a las personas
+		data = []
+		#print(idProductos)
+		for i in idProductos:
+			for producto in Producto.objects.filter(estado = True):
+				if int(i) == int(producto.id):
+					data.append(producto.fecha_registro)
+					data.append(producto.Lote)
+					for resultado in Resultado.objects.filter(estado = True).order_by('Elemento'):
+						if resultado.producto.id == producto.id:
+							data.append(resultado.Elemento.Abreviatura)
+							data.append(resultado.Resultado)
+		print(data)
+		detalles = [(producto.estado, producto.Lote,resultado.Elemento.Abreviatura) 
+					for i in idProductos
+						for producto in Producto.objects.filter(estado = True)
+							if int(i) == int(producto.id)
+								for resultado in Resultado.objects.filter(estado = True).order_by('Elemento')
+									if int(resultado.producto.id) == int(producto.id)
+							]
+		#Establecemos el tamaño de cada una de las columnas de la tabla
+		detalle_orden = Table([encabezados] + detalles, colWidths=[2 * cm, 5 * cm, 5 * cm, 2 * cm])
+		#Aplicamos estilos a las celdas de la tabla
+		detalle_orden.setStyle(TableStyle(
+			[
+				#La primera fila(encabezados) va a estar centrada
+				('ALIGN',(0,0),(3,0),'CENTER'),
+				#Los bordes de todas las celdas serán de color negro y con un grosor de 1
+				('GRID', (0, 0), (-1, -1), 1, colors.black), 
+				#El tamaño de las letras de cada una de las celdas será de 10
+				('FONTSIZE', (0, 0), (-1, -1), 10),
+			]
+		))
+		#Establecemos el tamaño de la hoja que ocupará la tabla 
+		detalle_orden.wrapOn(pdf, 800, 600)
+		#Definimos la coordenada donde se dibujará la tabla
+		detalle_orden.drawOn(pdf, 60,y)
+	def get(self, request, *args, **kwargs):
+		idProductos= kwargs.pop('idProductos')
+		#Indicamos el tipo de contenido a devolver, en este caso un pdf
+		response = HttpResponse(content_type='application/pdf')
+		#La clase io.BytesIO permite tratar un array de bytes como un fichero binario, se utiliza como almacenamiento temporal
+		buffer = io.BytesIO()
+		#Canvas nos permite hacer el reporte con coordenadas X y Y
+		pdf = canvas.Canvas(buffer)
+		#Llamo al método cabecera donde están definidos los datos que aparecen en la cabecera del reporte.
+		self.cabecera(pdf)
+		y = 600
+		#lla al metodo table
+		self.tabla(pdf, y, idProductos)
+		#Con show page hacemos un corte de página para pasar a la siguiente
+		pdf.showPage()
+		pdf.save()
+		pdf = buffer.getvalue()
+		buffer.close()
+		response.write(pdf)
+		return response
