1d23862d7961f848249c05929341808957d33e2d
PcDestok <codemar.potosi@gmail.com>
2020-07-04 12:00:43 -0400

    add the search engine by code

--- sistema_laboratorio/app/cliente/urls.py
+++ sistema_laboratorio/app/cliente/urls.py
@@ -31,4 +31,6 @@ urlpatterns = [
 #ingresos
 	path('detalle-ingreso-cliente/<int:ingreso_id>/',views.DetalleIngresoCliente),
 	path('new-producto-a-ingreso/<int:ingreso_id>/',views.NewProductIngreso),
+	
+	path('search-code',views.searchCode),
 ]
